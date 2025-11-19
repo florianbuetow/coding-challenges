e60c1993c69dcd4e03dfe541207ace0f5e23dd3e
florian <2320560+florianbuetow@users.noreply.github.com>
2025-11-19 11:32:14 +0100

    Add LC 3025 (medium)

---
+++ leetcode/medium/3025. Find the Number of Ways to Place People I.py
@@ -0,0 +1,19 @@
+# O(n*n) time and O(1) space
+# link: https://leetcode.com/problems/find-the-number-of-ways-to-place-people-i/
+
+class Solution:
+    def numberOfPairs(self, points: List[List[int]]) -> int:
+        result = 0
+        points.sort(key=lambda point: -point[1])
+        points.sort(key=lambda point: point[0])
+
+        for idx in range(len(points) - 1):
+            min_y, max_y = float('-inf'), points[idx][1]
+            for jdx in range(idx + 1, len(points)):
+                y = points[jdx][1]
+                if y > min_y and y <= max_y:
+                    result += 1
+                    min_y = y
+                if y == max_y:
+                    break
+        return result
