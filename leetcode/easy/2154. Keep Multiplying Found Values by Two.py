0d62a86fc3fc3d14871585c9b213ba65fe459d0b
florian <2320560+florianbuetow@users.noreply.github.com>
2025-11-19 10:12:17 +0100

    Add LC 2154 (easy) and update documentation

---
+++ leetcode/easy/2154. Keep Multiplying Found Values by Two.py
@@ -0,0 +1,9 @@
+# O(n) time and space
+# link: https://leetcode.com/problems/keep-multiplying-found-values-by-two/
+
+class Solution:
+    def findFinalValue(self, nums: List[int], original: int) -> int:
+        nums = set(nums)
+        while original in nums:
+            original *= 2
+        return original
