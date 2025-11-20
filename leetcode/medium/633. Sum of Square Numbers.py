85a7d7c71b41234bfff3860fbe170a342a989309
florian <2320560+florianbuetow@users.noreply.github.com>
2025-11-21 00:21:58 +0100

    Update LC 633 implementation with binary search approach

--- leetcode/medium/633. Sum of Square Numbers.py
+++ leetcode/medium/633. Sum of Square Numbers.py
@@ -1,17 +1,25 @@
-import math
-
+# O(sqrt(c) * log c) time and O(1) space
+# link: https://leetcode.com/problems/sum-of-square-numbers/
 
 class Solution:
     def judgeSquareSum(self, c: int) -> bool:
-        # O(log sqrt(c)) time and O(1) space
-        # link: https://leetcode.com/problems/sum-of-square-numbers/
-        low, high = 0, math.floor(c ** 0.5)
-        while low <= high:
-            summ = low ** 2 + high ** 2
-            if summ < c:
-                low += 1
-            elif summ > c:
-                high -= 1
-            elif summ == c:
+        def findB(target):
+            left, right = 0, target
+            while left <= right:
+                mid = (left + right) // 2
+                val = mid * mid
+                if val < target:
+                    left = mid + 1
+                elif val > target:
+                    right = mid - 1
+                else:
+                    return mid
+            return -1
+
+        a = 0
+        while 2 * a*a <= c:
+            b = findB(c - a*a)
+            if b >= 0 and a**2 + b**2 == c:
                 return True
+            a += 1
         return False
