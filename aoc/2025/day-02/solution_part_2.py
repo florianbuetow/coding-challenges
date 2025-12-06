5a9aba87756fd89886a202bb82150f1b2c419a76
florian <2320560+florianbuetow@users.noreply.github.com>
2025-12-06 13:29:13 +0100

    Add AoC 2025 Day 2

---
+++ aoc/2025/day-02/solution_part_2.py
@@ -0,0 +1,37 @@
+# O(?) time and O(n) space
+# link: https://adventofcode.com/2025/day/2
+
+class Solution:
+	def findInvalidIds(self, filename) -> int:
+		def getIdRanges():
+			id_ranges = []
+			with open(filename, 'r') as fh:
+				for line in fh:
+					line = line.strip()
+					for id_range in line.split(','):
+						if id_range:
+							id_range = [int(n) for n in id_range.split('-')]
+							id_ranges.append(id_range)
+			return id_ranges
+
+		def isValidId(id):			
+			id = str(id)
+			length = len(id)
+			for size in range(1, 1 + len(id) // 2):
+				if length % size != 0: continue
+				if id == id[:size] * (length // size):
+					return False		
+			return True
+
+		result = 0
+		for id_start, id_end in getIdRanges():
+			for id in range(id_start, id_end + 1):
+				if not isValidId(id):
+					result += id
+		return result
+
+if __name__ == '__main__':
+	s = Solution()
+	print(s.findInvalidIds('input_0.txt'))
+	print(s.findInvalidIds('input_1.txt'))
+
