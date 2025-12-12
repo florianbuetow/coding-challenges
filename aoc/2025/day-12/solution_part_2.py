5ebf1f9e3fbd6be5278e325f32bd86c23d50b9c3
florian <2320560+florianbuetow@users.noreply.github.com>
2025-12-12 19:30:41 +0100

    AoC 2025 Day 12 Part 2

---
+++ aoc/2025/day-12/solution_part_2.py
@@ -0,0 +1,10 @@
+# O(1) time and O(1) space
+# link: https://adventofcode.com/2025/day/12
+
+class Solution:
+	def computeMerryChristmas(self) -> str:
+		return "Merry Christmas!"
+
+if __name__ == '__main__':
+	s = Solution()
+	print(s.computeMerryChristmas())
