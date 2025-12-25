2787150c0316fdc2070aac0dd2929c1e90a1feef
Florian <2320560+florianbuetow@users.noreply.github.com>
2025-12-25 01:45:11 +0100

    Adding leetcode 3075 (medium)

--- leetcode/medium/3075. Maximize Happiness of Selected Children.py
+++ leetcode/medium/3075. Maximize Happiness of Selected Children.py
@@ -1,14 +1,22 @@
 from heapq import heapify, heappop
+
+# O(n + k log n) time and O(1) space
+# link: https://leetcode.com/problems/maximize-happiness-of-selected-children/
+
 class Solution:
     def maximumHappinessSum(self, happiness: List[int], k: int) -> int:
-        # O(n + k * log n) time and O(1) space
-        # link: https://leetcode.com/problems/maximize-happiness-of-selected-children/
         for i in range(len(happiness)):
             happiness[i] *= -1
         heapify(happiness)
 
         result = decrease = 0
-        for _ in range(k):
-            result += max(0, -heappop(happiness) - decrease)
+        while k and -happiness[0] > decrease:
+            happy = -heappop(happiness) - decrease
+            result += happy
             decrease += 1
+            k -= 1
+
+        for i in range(len(happiness)):
+            happy = happiness[i] - decrease
+            result += max(0, happy)
         return result
