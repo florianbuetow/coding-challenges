cdc16728b88ab2018443b13198af2a6b13e12276
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-01-09 18:58:29 +0100

    Adding leetcode 3783 (easy)

---
+++ leetcode/easy/3783. Mirror Distance of an Integer.py
@@ -0,0 +1,6 @@
+# O(log n) time and space
+# link: https://leetcode.com/problems/mirror-distance-of-an-integer
+
+class Solution:
+    def mirrorDistance(self, n: int) -> int:
+        return abs(n - int(str(n)[::-1]))
