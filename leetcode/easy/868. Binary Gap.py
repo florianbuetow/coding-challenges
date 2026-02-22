71f15c35195614405e7b58510a40afe2ca5899a0
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-02-22 02:12:53 +0100

    Adding leetcode 868 (easy)

---
+++ leetcode/easy/868. Binary Gap.py
@@ -0,0 +1,16 @@
+# O(log n) time and O(1) space
+# link: https://leetcode.com/problems/binary-gap/
+
+class Solution:
+    def binaryGap(self, n: int) -> int:
+        result = idx = 0
+        pos = []
+        while n:
+            if n % 2:
+                pos.append(idx)
+                if len(pos) == 2:
+                    result = max(result, abs(pos[0] - pos[1]))
+                    pos = [pos[1]]
+            idx += 1
+            n //= 2
+        return result
