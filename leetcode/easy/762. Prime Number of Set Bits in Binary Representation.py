33d52ccca5cda408cc42706be6c7c92987c5e633
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-02-22 02:18:36 +0100

    Adding leetcode 762 (easy)

---
+++ leetcode/easy/762. Prime Number of Set Bits in Binary Representation.py
@@ -0,0 +1,27 @@
+# O(n) time and O(1) space
+# link: https://leetcode.com/problems/prime-number-of-set-bits-in-binary-representation/
+
+from math import sqrt
+
+class Solution:
+    def countPrimeSetBits(self, left: int, right: int) -> int:
+        def count_bits(n):
+            bits = 0
+            while n > 0:
+                bits += n % 2
+                n = n // 2
+            return bits
+
+        def is_prime(n):
+            if n == 2: return True
+            if n % 2 == 0: return False
+            if n < 2: return False
+            for divisor in range(3, int(sqrt(n)) + 1):
+                if n % divisor == 0:
+                    return False
+            return True
+
+        result = 0
+        for n in range(left, right + 1):
+            result += [0, 1][is_prime(count_bits(n))]
+        return result
