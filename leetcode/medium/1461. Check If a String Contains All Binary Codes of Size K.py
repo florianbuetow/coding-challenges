d7e152c3c4f8905e7b174523a19cdefc98ed8de0
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-02-23 19:38:53 +0100

    Adding leetcode 1461 (medium)

---
+++ leetcode/medium/1461. Check If a String Contains All Binary Codes of Size K.py
@@ -0,0 +1,12 @@
+# O(n*n) time and space
+# link: https://leetcode.com/problems/check-if-a-string-contains-all-binary-codes-of-size-k/
+
+class Solution:
+    def hasAllCodes(self, s: str, k: int) -> bool:
+        # O(n*n) time and space
+        codes = set()
+        for i in range(len(s)-k+1):
+            codes.add(s[i:i+k])
+            if len(codes) == 2 ** k:
+                return True
+        return False
