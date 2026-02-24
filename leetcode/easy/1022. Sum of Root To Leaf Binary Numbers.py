a67d52dd28582f4ded2dc4a29539e3a971336c7b
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-02-24 01:50:20 +0100

    Adding leetcode 1022 (easy)

---
+++ leetcode/easy/1022. Sum of Root To Leaf Binary Numbers.py
@@ -0,0 +1,22 @@
+# O(n) time and space
+# link: https://leetcode.com/problems/sum-of-root-to-leaf-binary-numbers/
+
+
+class Solution:
+    def sumRootToLeaf(self, root: Optional[TreeNode]) -> int:
+        result = 0
+
+        def helper(node, val):
+            nonlocal result
+            val += node.val
+            if node.left or node.right:
+                val *= 2
+                if node.left:
+                    helper(node.left, val)
+                if node.right:
+                    helper(node.right, val)
+            else:
+                result += val
+
+        helper(root, 0)
+        return result
