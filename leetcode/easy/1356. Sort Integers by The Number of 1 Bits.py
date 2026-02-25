d6a06b6c1f7236aa5f5dda9fc6e490e5ff879e2c
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-02-25 01:15:48 +0100

    Adding leetcode 1356 (easy)

---
+++ leetcode/easy/1356. Sort Integers by The Number of 1 Bits.py
@@ -0,0 +1,27 @@
+# O(n log n) time and O(n) space
+# link: https://leetcode.com/problems/sort-integers-by-the-number-of-1-bits
+
+from collections import defaultdict
+
+class Solution:
+    def sortByBits(self, arr: List[int]) -> List[int]:
+        def countBits(n):
+            count = 0
+            while n:
+                count += n%2
+                n //= 2
+            return count
+
+        buckets = defaultdict(list)
+        for n in arr:
+            count = countBits(n)
+            buckets[count].append(n)
+
+        for count in buckets:
+            buckets[count].sort(reverse=True)
+
+        result = []
+        for count in range(max(buckets.keys())+1):
+            while buckets[count]:
+                result.append(buckets[count].pop())
+        return result
