8437f0aef88085efaf25684d6a337bcc2e3854fc
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-03-01 02:22:06 +0100

    Adding leetcode 1689 (medium)

---
+++ leetcode/medium/1689. Partitioning Into Minimum Number Of Deci-Binary Numbers.py
@@ -0,0 +1,13 @@
+# O(log n) time and O(1) space
+# link: https://leetcode.com/problems/partitioning-into-minimum-number-of-deci-binary-numbers/
+
+
+class Solution:
+    def minPartitions(self, n: str) -> int:
+        result = 0
+        for digit in n:
+            digit = int(digit)
+            result = max(result, digit)
+            if result == 9:
+                break
+        return result
