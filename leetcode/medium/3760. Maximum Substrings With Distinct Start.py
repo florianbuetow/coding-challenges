f2dfb573e70f1fced2baee9b4e0ea25067a625e0
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-03-19 13:20:51 +0100

    Adding leetcode 3760 (medium)

---
+++ leetcode/medium/3760. Maximum Substrings With Distinct Start.py
@@ -0,0 +1,6 @@
+# link: https://leetcode.com/problems/maximum-substrings-with-distinct-start/
+
+class Solution:
+    def maxDistinct(self, s: str) -> int:
+        # O(n) time and space
+        return len(set(s))
