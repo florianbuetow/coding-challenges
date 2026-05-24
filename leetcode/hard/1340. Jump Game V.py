08c33614b85f39e31fcf5c2f6a0bb1bfdb5e55e3
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-05-24 13:46:59 +0200

    Adding leetcode 1340 (hard)

---
+++ leetcode/hard/1340. Jump Game V.py
@@ -0,0 +1,20 @@
+# link: https://leetcode.com/problems/jump-game-v/
+
+class Solution:
+    def maxJumps(self, arr: List[int], d: int) -> int:
+        # O(n) time and space
+        @cache
+        def helper(i):
+            res = 1
+            for j in range(i + 1, min(i + d, len(arr) - 1) + 1):
+                if arr[j] >= arr[i]: break
+                res = max(res, 1 + helper(j))
+            for j in range(i - 1, max(0, i - d) - 1, -1):
+                if arr[j] >= arr[i]: break
+                res = max(res, 1 + helper(j))
+            return res
+
+        result = 0
+        for i in range(len(arr)):
+            result = max(result, helper(i))
+        return result
