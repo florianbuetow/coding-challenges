949a14c9f5f01084ed0849016d55dc2a3653c266
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-06-07 02:47:23 +0200

    Simplify leetcode 2196 solution

--- leetcode/medium/2196. Create Binary Tree From Descriptions.py
+++ leetcode/medium/2196. Create Binary Tree From Descriptions.py
@@ -1,20 +1,17 @@
 # link: https://leetcode.com/problems/create-binary-tree-from-descriptions/
 
-# Definition for a binary tree node.
-# class TreeNode:
-# def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
 from typing import List, Optional
 
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def createBinaryTree(self, descriptions: List[List[int]]) -> Optional[TreeNode]:
         # O(n) time and space
-        # link: https://leetcode.com/problems/create-binary-tree-from-descriptions/
-
-        # construct the tree
         nodes, parents = {}, {}
         for p_id, c_id, isLeft in descriptions:
             child = nodes.get(c_id, TreeNode(c_id))
@@ -27,7 +24,6 @@ class Solution:
             nodes[child.val] = child
             parents[child.val] = parent.val
 
-        # find the node that doesn't have a parent
         for node_id in nodes:
             if node_id not in parents:
                 return nodes[node_id]
