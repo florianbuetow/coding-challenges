3da94156a42cdb3873d73f029223ba851f5c6565
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-08-12 09:09:00 +0200

    Adding leetcode 2958 (medium)

--- leetcode/medium/2958. Length of Longest Subarray With at Most K Frequency.py
+++ leetcode/medium/2958. Length of Longest Subarray With at Most K Frequency.py
@@ -1,19 +1,16 @@
 # link: https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/
 
-from typing import List
-
-
 class Solution:
     def maxSubarrayLength(self, nums: List[int], k: int) -> int:
         # O(n) time and space
-        # link: https://leetcode.com/problems/length-of-longest-subarray-with-at-most-k-frequency/
-        result = 0
-        counter = {}
-        left = 0
-        for right, n in enumerate(nums):
-            counter[n] = counter.get(n, 0) + 1
-            while counter[n] > k and left <= right:
-                counter[nums[left]] -= 1
+        result = left = 0
+        window = defaultdict(int)
+        for right, curr in enumerate(nums):
+            window[curr] += 1
+            while window[curr] > k:
+                prev = nums[left]
+                window[prev] -= 1
                 left += 1
-            result = max(result, right-left+1)
+            window_size = right - left + 1
+            result = max(result, window_size)
         return result
