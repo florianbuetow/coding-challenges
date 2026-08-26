ace29647bb37ac2c8d2e53d01908d024560024fd
Florian <2320560+florianbuetow@users.noreply.github.com>
2026-08-26 02:15:03 +0200

    Adding leetcode 2904 (medium)

---
+++ leetcode/medium/2904. Shortest and Lexicographically Smallest Beautiful String.py
@@ -0,0 +1,27 @@
+# link: https://leetcode.com/problems/shortest-and-lexicographically-smallest-beautiful-string/
+
+class Solution:
+    def shortestBeautifulSubstring(self, s: str, k: int) -> str:
+        # O(n*k) time and O(n) space
+        result = None
+        left = right = ones = 0
+        while right < len(s):
+            c = s[right]
+            if c == '1': ones += 1
+            while ones > k or (ones == k and s[left] != '1'):
+                c = s[left]
+                if c == '1': ones -= 1
+                left += 1
+            right += 1
+            if ones == k:
+                word = s[left:right]
+                if result is None:
+                    result = word
+                elif right-left < len(result):
+                    result = word
+                elif len(result) > len(word):
+                    result = word
+                elif len(result) == len(word) and word < result:
+                    result = word
+
+        return result if result else ""
